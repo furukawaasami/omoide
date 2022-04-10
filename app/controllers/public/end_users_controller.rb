131c30d26dcd4827c3d502b4701f37b2a261499c
furukawaasami <furukawaasami321@gmail.com>
2022-04-10 08:43:18 +0000

    ユーザーshowOK

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,17 +1,9 @@
 class Public::EndUsersController < ApplicationController
   def show
-    @end_user = current_end_user
-    @posts = current_end_user.posts
+    @end_user = EndUser.find(params[:id])
+    @posts = @end_user.posts
   end
   def index
     @end_users = EndUser.all
-  end
-  def edit
-  end
-  def quit
-  end
-  def out
-  end
-  def update
   end
 end
