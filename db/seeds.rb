873ff2ebaa0f6c128d02495e15523cff61fda8cb
furukawaasami <furukawaasami321@gmail.com>
2022-04-21 10:46:28 +0000

    adminseed変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-  email: 'omoide@omoide.com',
+  email: 'omoide@omoide',
   password: 'omoideomoide',
   )
