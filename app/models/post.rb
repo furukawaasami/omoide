e5bf0023371fde332b8fbd06a8016762b4e56268
furukawaasami <furukawaasami321@gmail.com>
2022-04-25 15:22:01 +0000

    favorites_listレイアウト更新

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,6 +21,6 @@ class Post < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/logo_transparent.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize_to_fill: [width, height]).processed
   end
 end
